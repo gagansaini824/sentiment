9f6ab18231c126e59863ea4c6e8c6df53eb4dad3
gagansaini824 <gagansaini824@gmail.com>
2022-09-16 18:56:26 +0530

    Add files via upload

---
+++ sentimental_streamlit.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+    
+tokenizer = AutoTokenizer.from_pretrained("C:\\Users\\Gagandeep Singh\\Documents\\amazon-review-sentiment-analysis\\")
+model = AutoModelForSequenceClassification.from_pretrained("C:\\Users\\Gagandeep Singh\\Documents\\amazon-review-sentiment-analysis\\")
+
+# title and description
+st.header("""
+# Sentiment Analysis
+""")
+st.text("")
+st.text("")
+st.text("Works with English, Dutch, German, French, Spanish and Italian")
+st.text("")
+# search bar
+query = st.text_input("Find Sentiment!", "")
+st.text("")
+
+st.write(f"{query}")
+
+
+if query != "":
+    # encode the query as sentence vector
+    
+    tokenized_text = tokenizer(query, return_tensors='pt')
+    output = model(**tokenized_text)
+    
+    
+    outputs = output.logits.softmax(dim=-1).tolist()[0]
+    outputs = np.argmax(outputs)+1
+    outputs = "*"*outputs 
+
+    st.text("")
+    st.write("Sentiment score: "," ",f"{outputs}")
